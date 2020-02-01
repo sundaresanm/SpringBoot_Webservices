b314f8d9f74058403dfe566a483a16ecc4d2ae8d
Sundaresan Mariyappan <sms.sundaresan@gmail.com>
2020-02-01 13:32:20 +0530

    first commit

---
+++ src/main/java/config/StudentApplication.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
+//C:\Users\hp\.m2\repository\com\main\Student\0.0.1-SNAPSHOT\Student-0.0.1-SNAPSHOT.jar
